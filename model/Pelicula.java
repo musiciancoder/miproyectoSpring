09c0e9cbaca109b3ecf85e76f9f9f1d5ba039060
musiciancoder <45342398+musiciancoder@users.noreply.github.com>
2019-09-03 17:11:16 -0400

    Add files via upload

--- model/Pelicula.java
+++ model/Pelicula.java
@@ -2,18 +2,42 @@ package net.itinajero.app.model;
 
 import java.util.*;
 
-public class Pelicula { //PARA RE´PRESENTAR UNA PELICULA EN EXIBICION
+public class Pelicula { //ESTA ES NUESTRA CLASE DE MODELO 
 	
-	//notar que las variables aca declaradas se llaman igual que los names de los inputs
+	//notar que las variables aca declaradas se llaman igual que los names (o paths) de los inputs 
 	
 	private int id;
 	private String titulo;
-	private int duracion;
-	private String clasificacion;
+	private int duracion=100;
+	private String clasificacion="B";
 	private String genero;
 	private String imagen = "cinema.png"; //imagen por defecto
 	private Date fechaEstreno;
 	private String estatus="Activa";
+	
+	private Detalle detalle;//objeto compuesto
+	
+	
+	
+	public Pelicula() { //constructor por defecto. Si no se encuentra un objeto de tipo pelicula en el modelo (porque aun no hemos llenado el formulario), el constructor por defecto es llamado, asignando los valores por defecto que hemos dado si es que inicializamos alguna variable al inicializarla, como por ejemplo private int duracion=100;
+		System.out.println("Constructor Pelicula");
+		System.out.println("Pelicula: " + this.toString());
+	}
+	
+	
+	
+	public Detalle getDetalle() {
+		return detalle;
+	}
+
+
+
+	public void setDetalle(Detalle detalle) {
+		this.detalle = detalle;
+	}
+
+
+
 	public int getId() {
 		return id;
 	}
@@ -62,12 +86,14 @@ public class Pelicula { //PARA RE´PRESENTAR UNA PELICULA EN EXIBICION
 	public void setEstatus(String estatus) {
 		this.estatus = estatus;
 	}
+	
 	@Override
 	public String toString() {
 		return "Pelicula [id=" + id + ", titulo=" + titulo + ", duracion=" + duracion + ", clasificacion="
 				+ clasificacion + ", genero=" + genero + ", imagen=" + imagen + ", fechaEstreno=" + fechaEstreno
-				+ ", estatus=" + estatus + "]";
+				+ ", estatus=" + estatus + ", detalle=" + detalle + "]";
 	}
+
 	
 	
 
